de0a031536dec45d267a7bc2fe1371fc9a65d32d
therealfunkjoy <funkjoy@funkjoy.com>
2026-09-25 23:06:50 +0200

    Integrate Newsfeed into homepage

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,12 +1,13 @@
 import "./Home.css";
 import HomeNavigation from "../Components/HomeNavigation";
+import Newsfeed from "../Components/Newsfeed";
 
 export default function Home() {
     return (
         <main className="home">
             <section className="home__news">
                 <h1>Siste nytt</h1>
-                {/* Nyhetskomponenten legges inn her */}
+                <Newsfeed />
             </section>
 
             <HomeNavigation />
